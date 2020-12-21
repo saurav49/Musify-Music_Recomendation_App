af4751560bcce9d6e5b927e7cbb5801d9d87331c
Saurav Biswas <38159915+saurav49@users.noreply.github.com>
2020-12-21 13:59:39 +0600

    feature - added a play song section

--- src/Components/Favorites/Favorites.js
+++ src/Components/Favorites/Favorites.js
@@ -7,26 +7,54 @@ const genre = {
   PunkRock: [
     {
       band: "Green Day",
-      song: "Good Radiance (Time Of Your Life)"
+      song: "Good Radiance (Time Of Your Life)",
+      play:
+        "https://open.spotify.com/search/green%20day%20time%20of%20your%20life"
     },
     {
       band: "Blink 182",
-      song: "I Miss You"
+      song: "I Miss You",
+      play: "https://open.spotify.com/search/blink%20182%20i%20miss%20you"
     },
     {
       band: "BrainCoats",
-      song: "L-X"
+      song: "L-X",
+      play: "https://open.spotify.com/search/braincoats%20l-x"
     }
   ],
   Rap: [
-    { band: "TMG", song: "Walkman" },
-    { band: "Noel Miller", song: "LoopHole" },
-    { band: "Eminem", song: "Space Bound" }
+    {
+      band: "TMG",
+      song: "Walkman",
+      play: "https://open.spotify.com/search/tiny%20meat%20gang%20walkman"
+    },
+    {
+      band: "Noel Miller",
+      song: "LoopHole",
+      play: "https://open.spotify.com/search/noel%20miller%20loophole"
+    },
+    {
+      band: "Eminem",
+      song: "Space Bound",
+      play: "https://open.spotify.com/search/eminem%20space%20bound"
+    }
   ],
   Alternative: [
-    { band: "SoundGarden", song: "BlackHoleSun" },
-    { band: "Slipknot", song: "Duality" },
-    { band: "AudioSlave", song: "Like A Stone" }
+    {
+      band: "SoundGarden",
+      song: "BlackHoleSun",
+      play: "https://open.spotify.com/search/black%20hole%20sun"
+    },
+    {
+      band: "Slipknot",
+      song: "Duality",
+      play: "https://open.spotify.com/search/slipknot%20duality"
+    },
+    {
+      band: "AudioSlave",
+      song: "Like A Stone",
+      play: "https://open.spotify.com/search/audioslave%20like%20a%20stone"
+    }
   ]
 };
 
@@ -55,6 +83,10 @@ const Favorite = () => {
             <div className={styles.favoriteContainer}>
               <h2> {artist.band} </h2>
               <p> {artist.song} </p>
+              <p>
+                {" "}
+                <a href={artist.play}>Play</a>
+              </p>
             </div>
           );
         })}
